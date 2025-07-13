591fca28a73e680fe143c77edb74200379eadc24
pradeepbgs <kumarpradeepbgs@gmail.com>
2025-07-13 10:26:00 +0530

    aaye

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-
 	"github.com/joho/godotenv"
 	"github.com/pradeepbgs/pingfile/cmd"
 )
 
-func main(){
+func main() {
 	godotenv.Load()
-	// if err != nil {
-	// 	log.Println("No .env file found or error loading .env file")
-	// }
+
 	cmd.Execute()
 }
